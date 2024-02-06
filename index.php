c53bd827a303d49b794cd0e64f2e4a35ed437be0
Gustavo-erades <eradesvilarinho@gmail.com>
2024-02-06 19:42:12 -0300

    invertendo array com for

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php 
-    // algoritmo de ordenação de vetor bubble sort 
-    $array=[46,560,12,660,835];
-    for($i=0;$i<sizeof($array);$i+=1){
-        for($j=0;$j<sizeof($array)-$i-1;$j+=1){
-            if($array[$j]>$array[$j+1]){
-                $aux=$array[$j+1];
-                $array[$j+1]=$array[$j];
-                $array[$j]=$aux;
-            }
-        }
-    }
+    $array=[4,6,2,10];
     echo implode("-",$array);
-
+    echo "<br>";
+    $max=sizeof($array)-1;
+    $array2=[];
+    $cont=0;
+    for($i=$max;$i>=0;$i--){
+        $array2[$cont]=$array[$i];
+        $cont++;
+    }
+    echo implode("-",$array2);
