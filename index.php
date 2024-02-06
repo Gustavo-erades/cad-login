22004208fc2806bcd765086197f0b4d0aa030ae2
Gustavo-erades <eradesvilarinho@gmail.com>
2024-02-06 18:58:32 -0300

    algoritmo bubble sort

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php 
-    // algoritmo de ordenação de vetor selection sort 
-    $array=[6,4,9,8,34,55,99,2];
-    $tamanho_array=sizeof($array)-1;
-    $val_aux=null;
-    for($i=0;$i<=$tamanho_array;$i++){
-       for($j=$i+1;$j<=$tamanho_array;$j++){
-            if($array[$i]>$array[$j]){
-                $val_aux=$array[$j];
-                $array[$j]=$array[$i];
-                $array[$i]=$val_aux;
+    // algoritmo de ordenação de vetor bubble sort 
+    $array=[46,560,12,660,835];
+    for($i=0;$i<sizeof($array);$i+=1){
+        for($j=0;$j<sizeof($array)-$i-1;$j+=1){
+            if($array[$j]>$array[$j+1]){
+                $aux=$array[$j+1];
+                $array[$j+1]=$array[$j];
+                $array[$j]=$aux;
             }
-       }
+        }
     }
     echo implode("-",$array);
 
