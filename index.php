6ae7c372811906ec6a109c7cba73d4494135da8d
Gustavo-erades <eradesvilarinho@gmail.com>
2024-02-10 00:01:33 -0300

    iniciando front das criticas de cinema

--- index.php
+++ index.php
@@ -3,37 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="shortcut icon" href="./assets/imagens/icone-logo.png" type="image/x-icon">
-    <title>Faça sua critica</title>
+    <title>Criticas cinema</title>
 </head>
 <body>
-    <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <header class="navbar sticky-top bg-light ">
+        <nav class="container">
             <a href="" class="navbar-brand">
-                <img src="./assets/imagens/icone-logo.png" alt="" width="30" height="30">
                 Críticas de cinema
             </a>
-            <div class="navbar-nav">
-            <li class="nav-item">
-            <a class="nav-link">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link">About</a>
-          </li>
-
-            </div>
+            <button class="btn btn-outline-success me-2">
+                give your opnion
+            </button>
         </nav>
-        <div class="container">
-
+    </header>
+    <div class="container mt-3">
+        <div>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, sed ducimus sunt inventore commodi nisi obcaecati aliquid quisquam qui! Porro inventore perferendis dignissimos ipsum, veritatis omnis a et vero aliquam.</p>
+            <p class="lead">Officia maiores saepe quae, eum beatae esse sequi iste? Molestiae nihil perspiciatis cupiditate similique blanditiis! Perspiciatis labore quam distinctio maiores, omnis ipsum dolore enim? Eum molestias est laboriosam excepturi omnis!</p>
+            <p class="lead">Velit perferendis facilis tenetur dolorum cumque minima explicabo eius dolor, ipsam deleniti assumenda rem quaerat autem. Excepturi suscipit possimus sequi perspiciatis exercitationem consequuntur minus nisi quibusdam, voluptas aspernatur tempore totam?</p>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 mb-3 mb-md-0">
+                <div class="card text-bg-light">
+                    <div class="card-body">
+                        <h4 class="card-tittle">
+                            lorem tittle
+                        </h4>
+                        <div class="card-text">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate sed soluta qui adipisci hic accusamus illo! Cum eaque esse earum labore laborum sapiente amet ducimus quae. A dolore quae ratione?
+                        </div>
+                        <button class="btn btn-outline-primary mt-2">
+                            go to somewhere
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 mb-3 mb-md-0">
+                <div class="card text-bg-light">
+                    <div class="card-body">
+                        <h4 class="card-tittle">
+                            lorem tittle
+                        </h4>
+                        <div class="card-text">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate sed soluta qui adipisci hic accusamus illo! Cum eaque esse earum labore laborum sapiente amet ducimus quae. A dolore quae ratione?
+                        </div>
+                        <button class="btn btn-outline-warning mt-2">
+                            go to somewhere
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-
-
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <footer>
+        <span class="lead mb-2 mt-2">
+            &copy; development by Gustavo Êrades
+        </span>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js
+"></script>
 </body>
 </html>
