67c111c5c0f02ca150b981033a8a4a64adc0a10e
Gustavo-erades <eradesvilarinho@gmail.com>
2024-02-13 18:09:50 -0300

    corrigindo bug no js

--- assets/js/script_inputs.js
+++ assets/js/script_inputs.js
@@ -1,9 +1,9 @@
 const input_email=document.querySelector('#email');
 const input_senha=document.querySelector('#senha');
-const regexEmail= /^[A-z\.\-]+@([A-z]+\.)+[A-z]+$/g;
 function loginRegexEmail(){
-    let match=regexEmail.exec(input_email.value);
-    if(match!=null){
+    console.log("length: "+input_email.length);
+    console.log("value: "+input_email.value);
+    if(input_email.value!=""){
         input_email.style.backgroundColor="rgb(234, 255, 190)";
         input_email.style.borderColor="#b0ff4f";
     }else{
@@ -13,8 +13,6 @@ function loginRegexEmail(){
 }
 function loginRegexSenha(){
     let tam_input_senha=(input_senha.value).length;
-    console.log(tam_input_senha);
-    console.log(input_senha.value);
     if(tam_input_senha>=8){
         input_senha.style.backgroundColor="rgb(234, 255, 190)";
         input_senha.style.borderColor="#b0ff4f";
