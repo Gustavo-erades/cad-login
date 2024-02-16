86729b05cfebb190987e3620afbf5155b4182167
Gustavo-erades <eradesvilarinho@gmail.com>
2024-02-16 19:45:48 -0300

    testes com o ajax

--- assets/js/loadingAjax.js
+++ assets/js/loadingAjax.js
@@ -10,3 +10,15 @@ function chamarPagina(url){
     }
     ajax.send()
 }
+function chamarPagina2(url){
+    const ajax= new XMLHttpRequest();
+    ajax.open('GET', url);
+    ajax.onreadystatechange = () => {
+       if(ajax.readyState==4 && ajax.status==200){
+        document.querySelector("#conteudo").innerHTML='teste.html';
+       }else if(ajax.status==404){
+        window.location.href='erro404.html';
+       }
+    }
+    ajax.send()
+}
